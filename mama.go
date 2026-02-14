915104417ca0711e39dc383388aa82cdbf6bbba3
juric <ura@smartplus.ru>
2026-02-14 12:20:56 +0300

    mama file map

--- mama.go
+++ mama.go
@@ -1,13 +1,22 @@
 package main
 
+import "sync"
+
 type Mama struct {
 	Name string `json:"name"`
 	Age  int    `json:"age"`
 	Work string `json:"work"`
 }
 
-func defaultMama() map[string]Mama {
-	return map[string]Mama{
+// MamaMap is the map type that stores Mama entries keyed by ID.
+type MamaMap map[string]Mama
+
+// add map struct for Mama
+var mama = defaultMama()
+var mamaMu sync.RWMutex
+
+func defaultMama() MamaMap {
+	return MamaMap{
 		"1": {
 			Name: "Mama",
 			Age:  50,
@@ -28,10 +37,22 @@ func Maman() {
 // It creates a copy of the Mama data to provide a consistent view of the state
 // at a particular point in time. This is useful for preventing external modifications
 // to the internal Mama state.
+// insertMama adds or updates a Mama entry in the package-level map.
+func insertMama(id string, m Mama) {
+	mamaMu.Lock()
+	defer mamaMu.Unlock()
+	mama[id] = m
+}
+
 func snapshotMama() map[string]Mama {
-	// This function would be similar to snapshotArtists, but for Mama.
-	// It would return a copy of the current state of the Mama data.
-	return defaultMama() // Placeholder, as we don't have a mutable state for Mama in this example.
+	mamaMu.RLock()
+	defer mamaMu.RUnlock()
+
+	copyMama := make(map[string]Mama, len(mama))
+	for id, mm := range mama {
+		copyMama[id] = mm
+	}
+	return copyMama
 }
 
 // test snapshotMama
